e589cdcaaf1d71b6c7e1efb4968d101afb746c16
Brian Fiala <bgfiala@gmail.com>
2017-04-11 16:26:15 -0400

    fixed one type in directives file (changed function name from repos to repo to match directive declaration. Also, simplified html while maitaining the same effect.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ A small app to display github repos for practice at The Iron Yard.
 ## Getting Started
 
 Clone this repo, then run an http server in the `src/` directory. Be sure to type in GitHub [personal access token](https://github.com/settings/tokens) to authenticate with GitHub.
+  NOTE: you must have a GitHub account to get a personal access token.
+
+Contributors: Jordan Kasper and Brian Fiala, The Iron Yard, Front-end Engineering Course, Washington, D.C., Spring, 2017.
--- src/repos/repos.directive.js
+++ src/repos/repos.directive.js
@@ -5,17 +5,16 @@
   .directive('repo', repo);
 
   /**
-  * Creates the repos directive
-  * @return {Object} Object containing the template and scope of the directive
+  * Constructor for the repo directive
+  * @return {Object} Template and scope object representing the directive
   */
-  function repos() {
+  function repo() {
 
     return {
       templateUrl: 'repos/panel.template.html',
       restrict: 'EA',
       scope: {
-        repo: '=repo',
-        list: 'repos'
+        repo: '=repo'
       }
     };
   }
